faef48c663caf0bb26db8385276f149b68039379
Alexander Paul P. Quinit <paulquinit@gmail.com>
2019-10-30 17:28:41 +0800

    Update string

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,6 +2,6 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract HelloWorld {
   function helloWorld() public pure returns (string memory) {
-    return ("Hello World");
+    return ("Hello World!");
   }
 }
