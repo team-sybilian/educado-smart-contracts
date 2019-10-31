98f5d24bafaa7ca976a7e39a971303c70dcbd68e
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-10-31 17:14:21 +0800

    Upate return string

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,6 +2,6 @@ pragma solidity >=0.4.24 <0.6.0;
 
 contract HelloWorld {
   function helloWorld() public pure returns (string memory) {
-    return ("Hello World");
+    return ("Hello World!");
   }
 }
