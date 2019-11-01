81cc91fd6f17936e27bab45989e554cbbb0df85d
Paul Quinit <16703483+apquinit@users.noreply.github.com>
2019-11-01 08:53:35 +0800

    Update User.sol

--- contracts/User.sol
+++ contracts/User.sol
@@ -5,18 +5,18 @@ contract User {
     struct UserEntry {
         bytes32 checksum;
         string name;
-        string class;
+        string userType;
         uint timestamp;
         bool isSet;
         address setBy;
     }
 
     mapping (bytes32 => UserEntry) userMapping;
-    event NewUserEntry(bytes32 _checksum, string _name, string _class, uint _timestamp, address indexed _setBy);
+    event NewUserEntry(bytes32 _checksum, string _name, string _userType, uint _timestamp, address indexed _setBy);
     /**
      * Add a new entry to the ledger.
      *
-     * Example: 0x9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08, "Name", "Class", "1572506961"
+     * Example: 0x9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08, "Name", "Type", "1572506961"
      **/
     function createUserEntry(bytes32 _checksum, string memory _name, string memory _class, uint _timestamp) public {
         // Check if the entry does not exists.
@@ -24,7 +24,7 @@ contract User {
         // Set the default and input values of the entry.
         userMapping[_checksum].checksum = _checksum;
         userMapping[_checksum].name = _name;
-        userMapping[_checksum].class = _class;
+        userMapping[_checksum].userType = _userType;
         userMapping[_checksum].timestamp = _timestamp;
         userMapping[_checksum].isSet = true;
         userMapping[_checksum].setBy = msg.sender;
@@ -38,7 +38,7 @@ contract User {
         // Check if the entry exists.
         require(userMapping[_checksum].isSet, "User does not exists.");
         // Return entry.
-        return(userMapping[_checksum].checksum, userMapping[_checksum].name, userMapping[_checksum].class,
+        return(userMapping[_checksum].checksum, userMapping[_checksum].name, userMapping[_checksum].userType,
             userMapping[_checksum].timestamp, userMapping[_checksum].setBy);
     }
 }
